0d96848663b094d1e5a0fb103132827b513dfc9c
Zach Ruiz <powersbooth10@gmail.com>
2023-04-05 13:42:51 -0500

    added some javascript with a random number generator for dice.

---
+++ index.js
@@ -0,0 +1,8 @@
+// function rando() {
+//     let randomNumber1 = Math.random() * 6;
+// randomNumber1 = Math.floor(randomNumber1) + 1;
+// console.log(randomNumber1);
+// }
+let randomNumber1 = Math.random() * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+console.log(randomNumber1);
