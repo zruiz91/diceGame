b2deedc188ac7a76b31719ad79429211d691f3fc
Zach Ruiz <powersbooth10@gmail.com>
2023-04-06 12:04:18 -0500

    added JS code to change die image based on random number generator.

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-// function rando() {
-//     let randomNumber1 = Math.random() * 6;
-// randomNumber1 = Math.floor(randomNumber1) + 1;
-// console.log(randomNumber1);
-// }
-let randomNumber1 = Math.random() * 6;
+function rando() {
+    let randomNumber1 = Math.random() * 6;
 randomNumber1 = Math.floor(randomNumber1) + 1;
 console.log(randomNumber1);
+}
+
+
+let randomNumber1 = Math.random() * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+
+if (randomNumber1 === 1) {
+    document.querySelector("img.img1").setAttribute("src", "images/dice1.png");
+} else if (randomNumber1 === 2) {
+    document.querySelector("img.img1").setAttribute("src", "images/dice2.png");
+} else if (randomNumber1 === 3) {
+    document.querySelector("img.img1").setAttribute("src", "images/dice3.png");
+} else if (randomNumber1 === 4) {
+    document.querySelector("img.img1").setAttribute("src", "images/dice4.png");
+} else if (randomNumber1 === 5) {
+    document.querySelector("img.img1").setAttribute("src", "images/dice5.png");
+} else {
+    document.querySelector("img.img1").setAttribute("src", "images/dice6.png");
+}
+
+let randomNumber2 = Math.random() * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+
+if (randomNumber2 === 1) {
+    document.querySelector("img.img2").setAttribute("src", "images/dice1.png");
+} else if (randomNumber2 === 2) {
+    document.querySelector("img.img2").setAttribute("src", "images/dice2.png");
+} else if (randomNumber2 === 3) {
+    document.querySelector("img.img2").setAttribute("src", "images/dice3.png");
+} else if (randomNumber2 === 4) {
+    document.querySelector("img.img2").setAttribute("src", "images/dice4.png");
+} else if (randomNumber2 === 5) {
+    document.querySelector("img.img2").setAttribute("src", "images/dice5.png");
+} else {
+    document.querySelector("img.img2").setAttribute("src", "images/dice6.png");
+}
